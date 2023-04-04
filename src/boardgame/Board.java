407b6aa178b06009191305acc07c9bf0fd11a0cb
Luis Arf <lu_arf@yahoo.com.br>
2023-04-03 23:10:57 -0300

    Chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -11,6 +11,22 @@ public class Board {
 		pieces = new Piece[rows][columns];
 	}
 	
+	public Piece piece (int row,int column) {
+		return pieces[row][column];
+	}
+	
+	public Piece piece (Position pos) {
+		return piece(pos.getRow(),pos.getColumn());
+	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public int getColumns() {
+		return columns;
+	}
+	
 	
 
 }
