5763fb4d3c86cb95995a5b4e03b70a63a374d6d8
Luis Arf <lu_arf@yahoo.com.br>
2023-04-21 15:29:31 -0300

    queen

--- src/chess/pieces/Queen.java
+++ src/chess/pieces/Queen.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package chess.pieces;
 
 import boardgame.Board;
@@ -8,23 +5,16 @@ import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
-/**
- * @author LUIS ARF
- *
- */
-public class Bishop extends ChessPiece {
+public class Queen extends ChessPiece {
 
-	/**
-	 * @param board
-	 * @param color
-	 */
-	public Bishop(Board board, Color color) {
+	public Queen(Board board, Color color) {
 		super(board, color);
 	}
 
 	@Override
 	public boolean[][] possiblesMoves() {
-		boolean mat[][] = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		boolean mat[][] = new boolean[getBoard().getRows()][getBoard().getColumns()]; 
+		
 		Position p = new Position(0,0);
 		
 		// ---------------------------> a esquerda e acima
@@ -81,12 +71,66 @@ public class Bishop extends ChessPiece {
 		if( getBoard().positionExists(p) && isThereOpponentPiece(p) )
 			mat[p.getRow()][p.getColumn()] = true;
 		
+
+		//above (movimentos acima)
+		p.setValues(position.getRow()-1, position.getColumn());
+		
+		// enquanto a posicao existe e não tem uma peça
+		while( getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) ) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow()-1);
+		}
+		
+		// testa se existe mais uma posicao e se essa posicao é de uma peca adversaria
+		if( getBoard().positionExists(p) && isThereOpponentPiece(p) )
+			mat[p.getRow()][p.getColumn()] = true;
+		
+		
+		// left (Movimentos a esquerda
+		p.setValues(position.getRow(), position.getColumn()-1);
+		
+		// enquanto a posicao existe e não tem uma peça
+		while( getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) ) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setColumn(p.getColumn()-1);
+		}
+		
+		// testa se existe mais uma posicao e se essa posicao é de uma peca adversaria
+		if( getBoard().positionExists(p) && isThereOpponentPiece(p) )
+			mat[p.getRow()][p.getColumn()] = true;
+		
+		// Right (Movimentos a direita)
+		p.setValues(position.getRow(), position.getColumn()+1);
+		
+		// enquanto a posicao existe e não tem uma peça
+		while( getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) ) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setColumn(p.getColumn()+1);
+		}
+		
+		// testa se existe mais uma posicao e se essa posicao é de uma peca adversaria
+		if( getBoard().positionExists(p) && isThereOpponentPiece(p) )
+			mat[p.getRow()][p.getColumn()] = true;
+
+		//below (movimentos abaixo)
+		p.setValues(position.getRow()+1, position.getColumn());
+		
+		// enquanto a posicao existe e não tem uma peça
+		while( getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) ) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow()+1);
+		}
+		
+		// testa se existe mais uma posicao e se essa posicao é de uma peca adversaria
+		if( getBoard().positionExists(p) && isThereOpponentPiece(p) )
+			mat[p.getRow()][p.getColumn()] = true;
+
 		return mat;
 	}
-
-
-	@Override 
+	
+	@Override
 	public String toString() {
-		return "B";
+		return  "Q";
 	}
+
 }
